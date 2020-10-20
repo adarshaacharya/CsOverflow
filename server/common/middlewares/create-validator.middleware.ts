da5c4606150ec161f980533ed052300def2dfe2a
Aadarsha <connectwithadarsha@gmail.com>
2020-10-21 00:59:20 +0545

    refactor: use error middleware

--- server/common/middlewares/create-validator.middleware.ts
+++ server/common/middlewares/create-validator.middleware.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Response } from 'express';
 import * as Joi from 'joi';
-// import { BadRequest } from '../exceptions/';
+import  BadRequest  from '../exceptions/bad-request';
 
 export const createValidator = (schema: Joi.Schema, key: string = 'body') => (
   req: any,
-  res: Response,
+  _: Response,
   next: NextFunction
 ) => {
   const { error } = schema.validate(req[key]);
   if (error) {
-    // throw new BadRequest(error.details[0].message);
-    res.status(400).json({ msg: error.details[0].message });
-    return;
+    throw new BadRequest(error);
+    // res.status(400).json({ msg: error.details[0].message });
+    // return;
   }
   next();
 };
--- server/common/middlewares/errors.middleware.ts
+++ server/common/middlewares/errors.middleware.ts
@@ -3,5 +3,5 @@ import { Request, Response } from 'express';
 export const errorHandler = (error: any, _: Request, res: Response) => {
   console.log('Server error', error);
   res.statusCode = error.statusCode;
-  res.json({ msg: 'Server error' });
+  res.send(error);
 };
