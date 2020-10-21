27178bf33db0a744f3ee6e6729733d1bad34d802
Aadarsha <connectwithadarsha@gmail.com>
2020-10-21 12:54:10 +0545

    refactor: move posts code to services

--- server/modules/posts/posts.controller.ts
+++ server/modules/posts/posts.controller.ts
@@ -1,22 +1,19 @@
 import { AuthRequest } from '../../common/types/types';
-import { Response } from 'express';
-import { Posts } from './posts.model';
+import { NextFunction, Response } from 'express';
+import { postsService } from './posts.service';
 
 class PostsController {
-  public async createOne(req: AuthRequest, res: Response): Promise<void> {
+  public async createOne(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
     try {
       const { title, body } = req.body;
-
-      const post: Posts = new Posts({
+      const post = postsService.createOne({
         title,
         body,
         userId: req.user!.id, // from token
       });
-      await post.save();
       res.status(201).json(post);
     } catch (error) {
-      console.log(error.message);
-      res.status(500).json('Server error');
+      next(error);
     }
   }
 }
