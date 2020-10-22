f0f58a3f296bc82e087f3318a7ec084874bb8d30
Aadarsha <connectwithadarsha@gmail.com>
2020-10-22 09:23:21 +0545

    feat: add mockup for comments routes

--- server/modules/comments/comments.routes.ts
+++ server/modules/comments/comments.routes.ts
@@ -7,17 +7,25 @@ import { authJwt } from '../../common/middlewares/auth.middleware';
 export const router: Router = Router();
 
 /**
- * @method POST
+ * @method GET
  * @route /api/posts/comments/:id
  * @description : Fetch all comments of a post
  * @acces private
  */
-// router.post('/:id', commentsController.findAll);
+// router.get('/:id', commentsController.findAll);
 
 /**
  * @method POST
- * @route /api/posts
+ * @route /api/posts/comments/:id
  * @description : Create a new post by auth user
  * @acces private
  */
 router.post('/:id', [authJwt, createValidator(createCommentsDto)], commentsController.createOne);
+
+/**
+ * @method DELETE
+ * @route /api/posts/comments/:id
+ * @description : Delete a comment to a post
+ * @acces private
+ */
+// router.get('/:id', commentsController.deleteOne);
