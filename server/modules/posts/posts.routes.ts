05381b31e066726426ba50a46ac1191ec071cbb2
Aadarsha <connectwithadarsha@gmail.com>
2020-10-22 09:51:00 +0545

    feat: add mockup for post routes!

--- server/modules/posts/posts.routes.ts
+++ server/modules/posts/posts.routes.ts
@@ -7,6 +7,38 @@ import { createPostDto } from './posts.dtos';
 export const router: Router = Router();
 
 /**
+ * @method GET
+ * @route /api/posts
+ * @description : Fetch all posts
+ * @acces private
+ */
+// router.get('/', postsController.findAll);
+
+/**
+ * @method GET
+ * @route /api/posts
+ * @description : Fetch all posts sorted by maximum interactivity
+ * @acces private
+ */
+// router.get('/top', postsController.findAll);
+
+/**
+ * @method GET
+ * @route /api/posts/tag/:tagname
+ * @description : Fetch all posts of a specific tag
+ * @acces private
+ */
+// router.get('/tag/:tagname', postsController.findAll);
+
+/**
+ * @method GET
+ * @route /api/posts/:id
+ * @description : Fetch a single post
+ * @acces private
+ */
+// router.get('/:id', postsController.findOneById);
+
+/**
  * @method POST
  * @route /api/posts
  * @description : Create a new post by auth user
@@ -14,3 +46,11 @@ export const router: Router = Router();
  * @async
  */
 router.post('/', [authJwt, createValidator(createPostDto)], postsController.createOne);
+
+/**
+ * @method DELETE
+ * @route /api/posts/:id
+ * @description : Delete a post
+ * @acces private
+ */
+// router.delete('/:id', postsController.findAll);
