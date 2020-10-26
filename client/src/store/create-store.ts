b96b9653b2151bdf10971bcee07be5c4baae5a56
Aadarsha <connectwithadarsha@gmail.com>
2020-10-26 20:32:35 +0545

    feat(client): config redux dev tools extension

--- client/src/store/create-store.ts
+++ client/src/store/create-store.ts
@@ -1,1 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './modules/combine-reducer';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
