c29bbe4e2cd2aab2477ce976144bd8a1d69231a9
Aadarsha <connectwithadarsha@gmail.com>
2020-10-28 10:47:40 +0545

    feat: routes checked

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from 'antd';
 import 'antd/dist/antd.css';
+import { Router } from 'react-router-dom';
 import React from 'react';
+import { useRoutes } from 'routing/routes';
+import history from 'utils/history';
 
-function App() {
+const App: React.FC = () => {
+  const routes = useRoutes();
+
   return (
     <>
-      
+      <Router history={history}>
+        <>{routes}</>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <>Home Page</>;
+  return <h1>Home Page</h1>;
 };
 
 export default Home;
