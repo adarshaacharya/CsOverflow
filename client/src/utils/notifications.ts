ad2ab8278e615303e382a3da2852f311ae4c72dc
Aadarsha <connectwithadarsha@gmail.com>
2020-11-01 21:35:52 +0545

    feat: add notification utils

---
+++ client/src/utils/notifications.ts
@@ -0,0 +1,16 @@
+import { message, notification } from 'antd';
+
+export function displaySuccessNotification(message: string, description?: string) {
+  return notification['success']({
+    message,
+    description,
+    placement: 'topLeft',
+    style: {
+      marginTop: 50,
+    },
+  });
+}
+
+export function displayErrorMessage(error: string) {
+  return message.error(error);
+}
