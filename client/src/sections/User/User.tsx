b9e5d6f7ae7484b214c8b3249f5ab37913e2e0de
Aadarsha <connectwithadarsha@gmail.com>
2020-11-06 12:43:03 +0545

    feat: add user store types

--- client/src/sections/User/User.tsx
+++ client/src/sections/User/User.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const User = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return <div></div>;
+};
 
-export default User
+export default User;
---
+++ client/src/store/modules/users/users.types.ts
@@ -0,0 +1,3 @@
+export const GET_USER = '@@/auth/GET_USER';
+export const GET_USERS = '@@/auth/GET_USERS';
+export const USER_ERROR = '@@/auth/USER_ERROR';
