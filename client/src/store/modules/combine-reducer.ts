03622d60e0a9c145195f3e3e26a6a552d5823921
Aadarsha <connectwithadarsha@gmail.com>
2020-11-06 16:40:38 +0545

    feat: add user to store

--- client/src/store/modules/combine-reducer.ts
+++ client/src/store/modules/combine-reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './auth/auth.reducer';
 import { errorReducer } from './errors/errors.reducer';
+import { usersReducer } from './users/users.reducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  user: usersReducer,
   error: errorReducer,
 });
 
