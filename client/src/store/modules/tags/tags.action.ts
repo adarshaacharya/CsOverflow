26d29e7375a7e626c55dabb43eafa1862e05d447
Aadarsha <connectwithadarsha@gmail.com>
2020-11-13 02:45:38 +0545

    feat: add get tag action function

--- client/src/store/modules/tags/tags.action.ts
+++ client/src/store/modules/tags/tags.action.ts
@@ -1,6 +1,21 @@
 import { Dispatch } from 'redux';
 import Api from 'store/api';
-import { GET_TAGS, TagsAction, TAG_ERROR } from './tags.types';
+import { GET_TAG, GET_TAGS, TagsAction, TAG_ERROR } from './tags.types';
+
+export const getTag = (tagName: string) => async (dispatch: Dispatch<TagsAction>) => {
+  try {
+    const { data } = await Api.get(`/tags/${tagName}`);
+    dispatch({
+      type: GET_TAG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TAG_ERROR,
+      payload: error,
+    });
+  }
+};
 
 export const getTags = () => async (dispatch: Dispatch<TagsAction>) => {
   try {
