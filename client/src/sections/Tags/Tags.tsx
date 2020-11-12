2b3fc23c702486d09b9e5949ad6271ed38948ce6
Aadarsha <connectwithadarsha@gmail.com>
2020-11-13 00:12:15 +0545

    feat: add tags types

--- client/src/sections/Tags/Tags.tsx
+++ client/src/sections/Tags/Tags.tsx
@@ -1,14 +1,30 @@
-import { Layout } from 'antd';
+import { Divider, Layout, Typography } from 'antd';
 import Sidebar from 'lib/components/Sidebar';
+import { useScrollToTop } from 'lib/hooks';
 import React from 'react';
 
-const {Content} = Layout
+const { Content } = Layout;
+const { Title, Paragraph } = Typography;
+
 const Tags = () => {
+  useScrollToTop();
+
+  const tagsSectionElement = <></>;
   return (
     <>
       <Sidebar />
       <Content className="tags">
-        <h1>TAGS</h1>
+        <div className="tags__header">
+          <Title level={3} className="tags__header--title">
+            Tags
+          </Title>
+          <Paragraph type="secondary">
+            A tag is a keyword or label that categorizes your question with other, similar questions. Using the right
+            tags makes it easier for others to find and answer your question.
+          </Paragraph>
+        </div>
+        <Divider />
+        {tagsSectionElement}
       </Content>
     </>
   );
---
+++ client/src/store/modules/tags/tags.types.ts
@@ -0,0 +1,3 @@
+export const GET_TAG = '@@/tags/GET_TAG';
+export const GET_TAGS = '@@/tags/GET_TAGS';
+export const TAG_ERROR = '@@/tags/TAG_ERROR';
