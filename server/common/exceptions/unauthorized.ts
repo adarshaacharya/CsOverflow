4f81c5077e5484e9a661f389305212170a557a33
Aadarsha <connectwithadarsha@gmail.com>
2020-11-23 14:13:41 +0545

    fix: tsconfig setup

--- server/common/exceptions/unauthorized.ts
+++ server/common/exceptions/unauthorized.ts
@@ -1,7 +1,7 @@
 import { BaseHttpError } from './base-http-error';
 
 class Unauthorized extends BaseHttpError {
-  constructor(error) {
+  constructor(error: string) {
     super(401, error);
   }
 }
