cf2cb5fb27fa5595b6505a16879bf4c3db63d867
Aadarsha <connectwithadarsha@gmail.com>
2020-12-02 22:27:15 +0545

    feat: add express rate limiter

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import compression from 'compression';
 import express, { Response } from 'express';
 import path from 'path';
+import rateLimit from 'express-rate-limit';
 
 // routes
 import { router as usersRoutes } from './modules/users/users.routes';
@@ -22,6 +23,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(compression());
+
+// rateLimiter
+app.use(
+  '/api/',
+  rateLimit({
+    windowMs: 25 * 60 * 1000,
+    max: 500,
+    message: { error: 'Too many requests!, please try again after 25mins' },
+  })
+);
 
 // routes
 app.use('/api/users', usersRoutes);
